f20dd5f6abb760437682b24a3d0ec556f7917e8b
PrajwalSh0206 <prajwalshetty57@gmail.com>
2021-02-16 19:41:34 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio
+<p>A simple portfolio to describe about myself and to to experiment with it as well as to see it is responsive in all the devices</p>
---
+++ js/portfolio.js
@@ -0,0 +1,17 @@
+var i = 0;
+
+var txt = `Simplicity is way to express beautiful website`; 
+
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+if (i < txt.length) {
+document.getElementById("demo").innerHTML += txt.charAt(i);
+i++;
+setTimeout(typeWriter, speed);
+}
+}
+
+window.onload = function() {
+typeWriter();
+};
