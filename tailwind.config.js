7b7a78ee24f9900f403888b153c01195bbe25279
PrajwalSh0206 <prajwalshetty57@gmail.com>
2021-11-25 13:18:28 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,13 @@
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: true,
     content:['./*.html']},
   darkMode: false, // or 'media' or 'class'
   theme: {
-    minWidth: {
-      '14': '3.5rem',
-    },
     extend: {},
   },
   variants: {
     extend: {
-      scale: ['active'],
-      display:['group-hover']
     },
   },
   plugins: [],
