5111b55ea184b981bb4d097174ab4191fb745157
PrajwalSh0206 <prajwalshetty57@gmail.com>
2024-08-25 23:19:25 +0530

    skill added

--- src/components/Skills/Skill.tsx
+++ src/components/Skills/Skill.tsx
@@ -1,22 +1,23 @@
+import { skillList } from '../../props/data/skillList';
 import SkillCard from '../common/SkillCard';
+import Title from '../common/Title';
 import './Skills.scss';
 
 function Skill() {
   return (
-    <div className="bg-white p-20 space-y-5 text-gray-600">
-      <p className="text-5xl">My Skills</p>
+    <div
+      className="p-20 space-y-5 bg-gray-100 text-gray-600 items-start flex flex-col"
+      id="skills"
+    >
+      <Title title="My Skills" borderColor="bg-gray-600"></Title>
       <div className="container items-start">
-        <SkillCard percent="90%" title="HTML" width="w-11/12"></SkillCard>
-        <SkillCard percent="80%" title="CSS/SCSS" width="w-9/12"></SkillCard>
-        <SkillCard percent="70%" title="Javascript" width="w-7/12"></SkillCard>
-        <SkillCard percent="70%" title="Typescript" width="w-7/12"></SkillCard>
-        <SkillCard percent="90%" title="React Js" width="w-11/12"></SkillCard>
-        <SkillCard percent="80%" title="Docker" width="w-9/12"></SkillCard>
-        <SkillCard percent="50%" title="Git" width="w-6/12"></SkillCard>
-        <SkillCard percent="70%" title="MySql" width="w-7/12"></SkillCard>
-
-        <SkillCard percent="40%" title="PostgreSQL" width="w-5/12"></SkillCard>
-        <SkillCard percent="80%" title="Node Js" width="w-9/12"></SkillCard>
+        {skillList.map(skill => (
+          <SkillCard
+            percent={skill.percent}
+            title={skill.title}
+            width={skill.width}
+          ></SkillCard>
+        ))}
       </div>
     </div>
   );
