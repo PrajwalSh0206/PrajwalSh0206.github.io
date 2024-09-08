bf5d3014252a0cd14f802baad5efb6f164414255
PrajwalSh0206 <prajwalshetty57@gmail.com>
2024-09-08 19:09:44 +0530

    contact-sm-added

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -2,9 +2,9 @@ import Title from '../common/Title';
 
 export default function Contacts() {
   return (
-    <div className="w-full bg-gray-700 p-10 lg:p-20 flex items-start flex-col space-y-5 text-white" id="contacts">
+    <div className="w-full bg-gray-700 p-10 lg:p-20 flex items-center md:items-start flex-col space-y-5 text-white" id="contacts">
       <Title title="Contact Me" borderColor="bg-white"></Title>
-      <div className="space-x-5 flex w-full">
+      <div className="space-y-5 md:space-x-5 flex md:flex-row flex-col w-full">
         <input type="text" className="w-full p-3 outline-none focus:outline-white outline-gray-400 bg-gray-700 rounded-md " placeholder="Name" />
 
         <input type="text" className="w-full p-3 outline-none focus:outline-white outline-gray-400 bg-gray-700 rounded-md " placeholder="Email" />
