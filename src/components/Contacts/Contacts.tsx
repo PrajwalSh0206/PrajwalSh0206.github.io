5999656df22659fc6615c66dcc2e446e34b7b94a
PrajwalSh0206 <prajwalshetty57@gmail.com>
2024-09-08 20:10:20 +0530

    contact-fixes

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -4,7 +4,7 @@ export default function Contacts() {
   return (
     <div className="w-full bg-gray-700 p-10 lg:p-20 flex items-center md:items-start flex-col space-y-5 text-white" id="contacts">
       <Title title="Contact Me" borderColor="bg-white"></Title>
-      <div className="space-y-5 md:space-x-5 flex md:flex-row flex-col w-full">
+      <div className="space-y-5 md:space-y-0 md:space-x-5 flex md:flex-row flex-col w-full">
         <input type="text" className="w-full p-3 outline-none focus:outline-white outline-gray-400 bg-gray-700 rounded-md " placeholder="Name" />
 
         <input type="text" className="w-full p-3 outline-none focus:outline-white outline-gray-400 bg-gray-700 rounded-md " placeholder="Email" />
