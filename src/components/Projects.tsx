bee5c947731b10b1b67df538ce320c7428e95d94
PrajwalSh0206 <prajwalshetty57@gmail.com>
2024-09-08 20:09:39 +0530

    project-sm-added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,9 +6,9 @@ const Projects = () => {
     <div className="w-full flex flex-col space-y-5 items-center md:items-start p-10 lg:p-20 bg-gray-700 border-t-2 text-white border-gray-400" id="projects">
       <Title borderColor="bg-white" title="Projects"></Title>
 
-      <div className="w-full flex flex-col md:flex-row space-y-5 md:space-x-5">
+      <div className="w-full flex flex-col items-stretch md:flex-row space-y-5 md:space-y-0 md:space-x-5 space-x-0">
         {projectList.map(val => (
-          <a href={val.link} className="w-full md:w-6/12 p-5 rounded-md border-white bg-white shadow-md text-black border-2 flex space-x-3 items-center">
+          <a href={val.link} className="w-full md:w-6/12 p-5 rounded-md border-white grow bg-white shadow-md text-black border-2 flex space-x-3 items-start">
             <div className="flex flex-col space-y-2">
               <a href={val.link} target="_blank" className="text-xl border-b-2 border-gray-400">
                 {val.title}
